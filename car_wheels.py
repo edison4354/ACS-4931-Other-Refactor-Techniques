51eac9784e911a76db2c7bb94f535ad98ce7e3ba
Edison Li <edison4354@gmail.com>
2022-03-04 19:27:58 -0800

    Completed exercise 21

--- car_wheels.py
+++ car_wheels.py
@@ -8,7 +8,6 @@ class Car:
         # Each wheel has a single tpms attached to it. 
         # Thus, instead of having a list of tpms in 'Car' class
         # have each of the tpms in each 'Wheel'.
-        self.tpms_list = tpms_di  # Tire Pressure Monitoring System.
         self.wheels = wheels
         # Set wheels' car reference into each wheel.
         for w in wheels:
@@ -23,30 +22,29 @@ class Wheel:
     #       initilaize the 'Wheel' object or you can create
     #       a setter method to set the tpms of the wheel. (you can do 
     #       both of course.)
-    def __init__(self, car = None, wheel_location = None):
+    def __init__(self, car = None, wheel_location = None, tpms_serial_number = None):
         self.car = car
         self.wheel_location = wheel_location
+        self.tpms = Tpms(self, tpms_serial_number)
+
 
     def install_tire(self):
         print('remove old tube.')
          # TODO: Rewrite the following after moving tpms to the 'Wheel' class
-        print('cleaned tpms: ', 
-              self.car.tpms_di[self.wheel_location].get_serial_number, 
-              '.')
+        print('cleaned tpms: ', self.tpms.get_serial_number(), '.')
         print('installed new tube.')        
         
     def read_tire_pressure(self):
         # TODO: After making tpms an attribute of 'Wheel' class,
         #       rewrite the following.
-        return self.car.tpms_di[self.wheel_location].get_pressure()
+        return self.tpms.get_pressure()
     
     def set_car(self, car):
         self.car = car
 
 
 class Tpms:
-    """Tire Pressure Monitoring System.
-    """
+    """Tire Pressure Monitoring System."""
     def __init__(self, serial_number):
         self.serial_number = serial_number
         self.sensor_transmit_range = 300 # [feet]
